eb0684267256d9ce1b845c4b000bb9863f13c347
msi-pc <18203447319@163.com>
2023-03-08 21:03:01 +0800

    first commit

---
+++ src/main/java/com/fanlj/demo/gitstudy/DemoGitStudyApplication.java
@@ -0,0 +1,14 @@
+package com.fanlj.demo.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitStudyApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        SpringApplication.run(DemoGitStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fanlj/demo/gitstudy/DemoGitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanlj.demo.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
