3fbdd3a4d907ead1626552bcc1dc4a83e0b03cc7
msi-pc <18203447319@163.com>
2023-03-08 22:00:20 +0800

    third5 commit

--- src/main/java/com/fanlj/demo/gitstudy/DemoGitStudyApplication.java
+++ src/main/java/com/fanlj/demo/gitstudy/DemoGitStudyApplication.java
@@ -11,6 +11,7 @@ public class DemoGitStudyApplication {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hello git5");
         SpringApplication.run(DemoGitStudyApplication.class, args);
     }
 
