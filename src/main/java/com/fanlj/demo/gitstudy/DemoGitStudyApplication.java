0f5d8cf3f777be621af1cee9f5dc7f5c46dd81b2
msi-pc <18203447319@163.com>
2023-03-08 22:15:23 +0800

    third8 commit master test!

--- src/main/java/com/fanlj/demo/gitstudy/DemoGitStudyApplication.java
+++ src/main/java/com/fanlj/demo/gitstudy/DemoGitStudyApplication.java
@@ -13,6 +13,8 @@ public class DemoGitStudyApplication {
         System.out.println("hello git4");
         System.out.println("hello git5");
         System.out.println("hello git6 hot-fix");
+        System.out.println("hello git7 master-test");
+
         SpringApplication.run(DemoGitStudyApplication.class, args);
     }
 
